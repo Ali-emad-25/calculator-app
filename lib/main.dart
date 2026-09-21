280c20c579e1075d88152800f7bf16b6c357888a
Ali-Emad <ali.emad.dev@gmail.com>
2026-09-21 23:44:35 +0300

    feat: update app logo and add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
-        scaffoldBackgroundColor: Colors.black,
+        scaffoldBackgroundColor: Color(0xFF000000),
       ),
       home: CalculatorScreen(),
     );
